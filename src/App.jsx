c7e54f43bde0422d633a10770d8356c40b77f896
Santiago Hernandez <smhg15@gmail.com>
2024-07-26 21:04:56 +0200

    feat(App): add loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import reñe from '/static/logoReñeSF.png'
 import Cocktails from './CocktailsNew'
 import InstagramIcon from '@mui/icons-material/Instagram';
-import { IconButton, Link, Typography } from '@mui/material';
+import { Box, IconButton, Link, Modal, Typography } from '@mui/material';
 
+const style = {
+  position: 'absolute',
+  width: '100vw',
+  height: '100vh',
+  bgcolor: '#000000',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: '#fafafa',
+};
 function App() {
+  const [open, setOpen] = useState(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    // Simulate an API call
+    setTimeout(() => {
+      handleClose()
+    }, 2000);
+  }, []);
+
   return (
+    <div>
     <div className="container-blackboard">
       <IconButton
         size='small'
@@ -34,6 +55,14 @@ function App() {
         </Link>
       </footer>
     </div>
+    <Modal
+      open={open}
+    >
+      <Box sx={style}>
+        <h2>Loading...</h2>
+      </Box>
+    </Modal>
+  </div>
 
   )
 }
